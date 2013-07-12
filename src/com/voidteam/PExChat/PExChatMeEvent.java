9f65aa9a335136c48bd527cf8076cde2e1f07746
Jack Klink <jacklinkproductions@mac.com>
2013-07-12 17:51:15 -0400

    Initial Commit
    PexChat Decompiled

---
+++ src/com/voidteam/PExChat/PExChatMeEvent.java
@@ -0,0 +1,34 @@
+package com.voidteam.PExChat;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PExChatMeEvent extends Event
+{
+  private static final HandlerList handlers = new HandlerList();
+private String message;
+private Player player;
+
+public PExChatMeEvent(Player player, String message)
+{
+	this.player = player;
+    this.message = message;
+}
+
+public String getMessage() {
+  return this.message;
+}
+
+public Player getPlayer() {
+  return this.player;
+}
+public HandlerList getHandlers()
+{
+  return handlers;
+}
+
+public static HandlerList getHandlerList() {
+  return handlers;
+}
+}
